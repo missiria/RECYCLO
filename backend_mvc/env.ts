082198e62839959e0c8e919e79c7db43bc085d05
missiria <missiria@gmail.com>
2022-03-09 00:05:23 +0100

    Auth is done // API

--- backend_mvc/env.ts
+++ backend_mvc/env.ts
@@ -21,6 +21,7 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  SESSION_DRIVER: Env.schema.string(),
 
   MYSQL_HOST: Env.schema.string({ format: 'host' }),
   MYSQL_PORT: Env.schema.number(),
--- backend_mvc/start/routes.ts
+++ backend_mvc/start/routes.ts
@@ -28,7 +28,6 @@ Route.get('/', async () => {
 Route.post('login', async ({ auth, request }) => {
   const email = request.input('email')
   const password = request.input('password')
-
   await auth.use('web').attempt(email, password)
 })
 
