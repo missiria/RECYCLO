f7df8416998a71d661592ec41562495d639a4116
missiria <missiria@gmail.com>
2022-03-11 21:59:52 +0100

    Init Account crud & validators // API

--- backend_mvc/app/Controllers/Http/AccountsController.ts
+++ backend_mvc/app/Controllers/Http/AccountsController.ts
@@ -1,3 +1,35 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Account from 'App/Models/Account'
+export default class AccountsController {
 
-export default class AccountsController {}
+  public async index({ response }) {
+    const users = await Account.all()
+    return response.ok(users)
+  }
+
+  public async show({ params, response }) {
+    const { id }: { id: Number } = params
+
+    const account: any = await Account.find(id)
+
+    if (!account) {
+      return response.notFound({ message: 'Compte none trouvé' })
+    }
+
+    return response.ok(account)
+  }
+
+  public async destroy({ params, response }) {
+    const { id }: { id: Number } = params
+    const account: any = await Account.find(id)
+
+    if (!account) {
+      return response.notFound({ message: "Compte none trouvé" })
+    }
+
+    await account.delete()
+
+    return response.ok({ message: 'Compte est bien supprimer successfully.' })
+  }
+
+}
