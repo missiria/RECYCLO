d2d79ed729265dd001be7838d94f74b28d8938a6
missiria <missiria@gmail.com>
2022-03-13 01:30:18 +0100

    Init Faker User data // API

--- backend_mvc/database/factories/index.ts
+++ backend_mvc/database/factories/index.ts
@@ -1,1 +1,17 @@
 // import Factory from '@ioc:Adonis/Lucid/Factory'
+import User from 'App/Models/User'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export const UserFactory = Factory
+  .define(User, ({ faker }) => {
+    return {
+      phone: faker.phone.phoneFormats(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      first_name: faker.name.findName(),
+      last_name: faker.name.lastName(),
+      active: faker.datatype.boolean(),
+      remember_me_token: faker.datatype.boolean()
+    }
+  })
+  .build()
