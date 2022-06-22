dce0952a09bc17720fa7de833fd8ec7c38c21426
= <abdellah.elkadiri.dev@gmail.com>
2022-06-22 16:22:10 +0100

    auth //4h

--- mobile/src/screens/collectors/profileDetails/services/adress.services.js
+++ mobile/src/screens/collectors/profileDetails/services/adress.services.js
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import {getData,storeData} from "../../../../hooks/hooks";
 
 export const defaultValues = {
     city: "",
@@ -12,5 +13,15 @@ export const schema = yup.object().shape({
 
 export const handleRegister = async (userData, navigation, setErrors) => {
     // console.log("userData", userData);
+    const user = await getData('user');
+
+    
+    const response = await apiClient.post("accounts", {
+
+        'city' : userData.city,
+        'address' : userData.neighborhood,
+    });
+
+    
     navigation.navigate("ChooseTypeIdentityConfirmation");
 };
