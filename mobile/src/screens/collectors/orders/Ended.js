68c0fc0b2a8d6305b2a6fc573c7867f8bf0d6e4c
MISSIRIA <missiria@gmail.com>
2023-02-14 00:54:09 +0100

    Fix bug orders & add message (#9)

--- mobile/src/screens/collectors/orders/Ended.js
+++ mobile/src/screens/collectors/orders/Ended.js
@@ -1,56 +1,67 @@
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
-import React,{useState,useEffect} from 'react';
-import Icon from 'react-native-vector-icons/Entypo';
-import Icond from 'react-native-vector-icons/FontAwesome';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import i18n from "i18next";
 
 import { useAPI } from "~/hooks/hooks";
-import { EdgeCardOrder } from '~/ui/cards/EdgeCardOrder';
-import { useFetch } from '../../../hooks/hooks';
+import { EdgeCardOrder } from "~/ui/cards/EdgeCardOrder";
+import { useFetch } from "../../../hooks/hooks";
 
 export default function Ended() {
-
   const [orders, setOrders] = useState([]);
 
-  const { isLoading, error, data, refetch } = useFetch(
-    'orders',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        status: 'DONE'
-      }),
-    }
-  );
+  const { isLoading, error, data, refetch } = useFetch("orders", {
+    method: "POST",
+    body: JSON.stringify({
+      status: "DONE",
+    }),
+  });
 
   useEffect(() => {
-    if (data !== null){
+    if (data !== null) {
       setOrders(data);
     }
   }, [isLoading]);
 
   useEffect(() => {
-    refetch()
-  }, [])
+    refetch();
+  }, []);
 
   return (
     <View style={styles.container}>
       <ScrollView>
-      { error !== null ? <Text>{error.message}</Text> : 
-          isLoading ? 
-            <ActivityIndicator size="small" color="#ff00ff" />
-          :
-          orders != undefined && orders.map((order) => (
-            <EdgeCardOrder key={order.id} order={order} onPressDelete={()=>{console.log("Delete")}} />
-          )) 
-        }
+        {error ? (
+          <Text>{error.message}</Text>
+        ) : isLoading ? (
+          <ActivityIndicator size="large" color="#ff00ff" />
+        ) : orders ? (
+          orders.length > 0 ? (
+            orders.map((order) => (
+              <EdgeCardOrder
+                key={order.id}
+                order={order}
+                onPressDelete={() => {
+                  console.log("Delete");
+                }}
+              />
+            ))
+          ) : (
+            <Text>No DONE order yet!</Text>
+          )
+        ) : null}
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-  }
-})
+    backgroundColor: "white",
+  },
+});
--- mobile/src/screens/collectors/orders/InProgress.js
+++ mobile/src/screens/collectors/orders/InProgress.js
@@ -1,75 +1,95 @@
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
-import React,{useState,useEffect} from 'react';
-import Icon from 'react-native-vector-icons/Entypo';
-import Icond from 'react-native-vector-icons/FontAwesome';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import i18n from "i18next";
 
 import { useAPI } from "~/hooks/hooks";
-import { EdgeCardOrder } from '~/ui/cards/EdgeCardOrder';
-import { useFetch } from '../../../hooks/hooks';
+import { EdgeCardOrder } from "~/ui/cards/EdgeCardOrder";
+import { useFetch } from "../../../hooks/hooks";
 
 export default function InProgress() {
-  
   const [orders, setOrders] = useState([]);
-  console.log(orders)
-  const { isLoading, error, data } = useAPI({
-    url: 'orders',
-    method: 'POST',
-    data: {
-      status: 'PENDING'
-    },
-  },true);
 
-  console.log(data)
+  const { isLoading, error, data } = useAPI(
+    {
+      url: "orders",
+      method: "POST",
+      data: {
+        status: "PENDING",
+      },
+    },
+    true
+  );
 
   // * Cancel Order
-  const [trigger, { data: message, isLoading: isOrderUpdateLoading }] = useFetch(undefined, {
-    method: "PUT",
-    body: JSON.stringify({
-      status: "CANCELED"
-    })
-  }, true)
+  const [trigger, { data: message, isLoading: isOrderUpdateLoading }] =
+    useFetch(
+      undefined,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          status: "CANCELED",
+        }),
+      },
+      true
+    );
 
+  console.log("In Progress");
+  console.log(data);
   console.log(message);
 
   useEffect(() => {
-    if (data !== null){
+    if (data) {
       setOrders(data);
     }
   }, [isLoading]);
-  
+
   const textAction = i18n.t("menageDemend.notReady");
 
   return (
     <View style={styles.container}>
       <ScrollView>
-      { error !== null ? <Text>{error.message}</Text> : 
-          isLoading ? 
-            <ActivityIndicator size="small" color="#ff00ff" />
-          :
-          orders?.length !==0 && orders?.map((order) => (
-            <EdgeCardOrder 
-              key={order.id} 
-              order={order} 
-              textAction={textAction} 
-              styleAction={styles.actionNotReady} 
-              onPressAction={() => trigger(`orders/${order.declaration_id}/update`)} 
-              onPressEdit={() => {console.log("Edit")}} 
-            />
-          )) 
-        }
+        {error ? (
+          <Text>{error.message}</Text>
+        ) : isLoading ? (
+          <ActivityIndicator size="large" color="#ff00ff" />
+        ) : orders ? (
+          orders.length !== 0 ? (
+            orders.map((order) => (
+              <EdgeCardOrder
+                key={order.id}
+                order={order}
+                textAction={textAction}
+                styleAction={styles.actionNotReady}
+                onPressAction={() =>
+                  trigger(`orders/${order.declaration_id}/update`)
+                }
+                onPressEdit={() => {
+                  console.log("Edit");
+                }}
+              />
+            ))
+          ) : (
+            <Text>No INPROGRESS order yet!</Text>
+          )
+        ) : null}
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
-  actionNotReady:{
-    backgroundColor: '#5B68F6',
-    color:'#fff'
-  }
-})
+  actionNotReady: {
+    backgroundColor: "#5B68F6",
+    color: "#fff",
+  },
+});
