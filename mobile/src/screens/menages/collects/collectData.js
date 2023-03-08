00e69ec9a651b5e79e725b5573591b3b7517bb3e
Hamza LACHQAR <lachquarhamza@gmail.com>
2023-03-08 21:44:45 +0100

    Edge 230216 order confirmation (#36)
    * clean -order accepted- file in collectos module
    
    * cleanup menage -> collects + add collectDetails service + add 2 hooks to get days, months
    
    * restore app.json to avoid merge conflict

--- mobile/src/screens/menages/collects/collectData.js
+++ mobile/src/screens/menages/collects/collectData.js
@@ -1,38 +1,32 @@
 export const moreCollects = [
-    {
-        id: 1,
-        name: 'Graines Citrouille',
-        img: require('../../../assets/images/a1.png'),
-        
-    },
-    {
-        id: 2,
-        name: "Noyau D'avocat",
-        img: require('../../../assets/images/a2.png'),
-        
-    },
-    {
-        id: 3,
-        name: 'Akshod',
-        img: require('../../../assets/images/a3.png'),
-        
-    },
-    {
-        id: 4,
-        name: 'Pneus',
-        img: require('../../../assets/images/a4.png'),
-        
-    },
-    {
-        id: 5,
-        name: 'Ahidor',
-        img: require('../../../assets/images/a5.png'),
-        
-    },
-    {
-        id: 6,
-        name: 'Moquette-Tapis',
-        img: require('../../../assets/images/a6.png'),
-        
-    },
-]
+  {
+    id: 1,
+    name: "Graines Citrouille",
+    img: require("../../../assets/images/a1.png"),
+  },
+  {
+    id: 2,
+    name: "Noyau D'avocat",
+    img: require("../../../assets/images/a2.png"),
+  },
+  {
+    id: 3,
+    name: "Akshod",
+    img: require("../../../assets/images/a3.png"),
+  },
+  {
+    id: 4,
+    name: "Pneus",
+    img: require("../../../assets/images/a4.png"),
+  },
+  {
+    id: 5,
+    name: "Ahidor",
+    img: require("../../../assets/images/a5.png"),
+  },
+  {
+    id: 6,
+    name: "Moquette-Tapis",
+    img: require("../../../assets/images/a6.png"),
+  },
+];
