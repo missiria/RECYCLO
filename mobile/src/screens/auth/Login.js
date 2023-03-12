53a255954955b2ef130573882415979c27c27566
Amine Amazzal <72992167+AMAZZAL-AMINE@users.noreply.github.com>
2023-03-12 20:03:48 +0100

    Edge 230221  rtl not applicable to input in the login screen (#17)
    * init fix input login and register rtl
    
    * fix login and register screen RTL
    
    ---------
    
    Co-authored-by: missiria <missiria@gmail.com>

--- mobile/src/screens/auth/Login.js
+++ mobile/src/screens/auth/Login.js
@@ -8,7 +8,7 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { Formik } from "formik";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Services
 import { handleLogin, schema, defaultValues } from "./services/login.services";
@@ -16,13 +16,24 @@ import { EdgeTextInput } from "~/ui/inputs/EdgeTextInput";
 
 export default function Login({ navigation }) {
   const [authLoaded, setAuthLoaded] = useState(false);
+    const [rtl, setRtl] = useState(false);
+    const rtlText = rtl && { textAlign: 'right', paddingRight: 20 };
+    const errorsRtlText = rtl && { textAlign: 'right', paddingRight: 0 };
+    useEffect(() => {
+        if (i18n.language == "ar") {
+            setRtl(true);
+        } else {
+            setRtl(false);
+        }
+    }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.textTitle}>
-        <Text style={styles.connectTitle}>Se connecter</Text>
-        <Text style={styles.connectDesc}>Avez-vous un compte? Connecter</Text>
-      </View>
-      <View style={styles.form}>
+      <ScrollView
+        contentContainerStyle={{ flex: 1, justifyContent: 'center' }}>
+          <View style={styles.textTitle}>
+            <Text style={styles.connectTitle}>Se connecter</Text>
+            <Text style={styles.connectDesc}>Avez-vous un compte? Connecter</Text>
+          </View>
         <Formik
           initialValues={defaultValues}
           validationSchema={schema}
@@ -31,18 +42,18 @@ export default function Login({ navigation }) {
           }
         >
           {(props) => (
-            <ScrollView>
+            <View>
               <EdgeTextInput
                 name="phone"
                 props={props}
-                style={styles.inputPhone}
+                 style={[styles.inputPassword, rtlText]}
                 keyboardType="phone-pad"
                 placeholder={i18n.t("login.phone")}
               />
               <EdgeTextInput
                 name="password"
                 props={props}
-                style={styles.inputPhone}
+                 style={[styles.inputPassword, rtlText]}
                 placeholder={i18n.t("login.password")}
               />
               <Text
@@ -65,19 +76,18 @@ export default function Login({ navigation }) {
                   i18n.t("login.sign_in")
                 )}
               </Text>
-              <Text style={styles.textRegister}>
+              <Text style={styles.textSignUp}>
                 {i18n.t("login.need_account")}
                 <Text
                   onPress={() => navigation.navigate("Register")}
-                  style={styles.signUpTextLink}
-                >
+                  style={styles.textSignUpLink}>
                   {i18n.t("login.sign_up")}
                 </Text>
               </Text>
-            </ScrollView>
+            </View>
           )}
         </Formik>
-      </View>
+      </ScrollView>
       <View style={styles.textBottom}>
         <Text style={styles.textBottomDesc}>{i18n.t("login.bottom_text")}</Text>
       </View>
@@ -87,82 +97,106 @@ export default function Login({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-  textTitle: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  connectTitle: {
-    fontWeight: "bold",
-    color: "black",
-    fontSize: 30,
-  },
-  connectDesc: {
-    color: "#767676",
-  },
-  form: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputPhone: {
-    backgroundColor: "#F6F6F6",
-    padding: 10,
-    textAlign: "left",
-    paddingLeft: 20,
-    paddingRight: 80,
-    borderRadius: 5,
-  },
-  inputPassword: {
-    backgroundColor: "#F6F6F6",
-    padding: 10,
-    textAlign: "left",
-    paddingLeft: 20,
-    paddingRight: 80,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  forgetCode: {
-    color: "#33CC66",
-    textAlign: "right",
-  },
-  buttonLogin: {
-    backgroundColor: "#33CC66",
-    padding: 15,
-    textAlign: "left",
-    paddingLeft: 80,
-    paddingRight: 80,
-    borderRadius: 5,
-    marginTop: 20,
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  textRegister: {
-    textAlign: "center",
-    fontSize: 12,
-    color: "#A3A3A3",
-    marginTop: 10,
-  },
-  signUpTextLink: {
-    color: "#33CC66",
-    fontWeight: "500",
-  },
-  textBottom: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        width: '100%',
+        paddingHorizontal: 30,
+    },
+    textTitle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 50,
+    },
+    connectTitle: {
+        color: 'black',
+        fontSize: 30,
+        letterSpacing: 0.5,
+    },
+    connectDesc: {
+        color: '#A3A3A3',
+        fontSize: 14,
+        marginTop: 10,
+        textAlign: 'center',
+        letterSpacing: 0.5,
+    },
+    inputPhone: {
+        backgroundColor: '#F6F6F6',
+        padding: 10,
+        textAlign: 'left',
+        paddingLeft: 20,
+        paddingRight: 80,
+        borderRadius: 5,
+        height: 55,
+        width: '100%',
+        letterSpacing: 0.5,
+    },
+    inputPassword: {
+        backgroundColor: '#F6F6F6',
+        padding: 10,
+        textAlign: 'left',
+        paddingLeft: 20,
+        paddingRight: 80,
+        borderRadius: 5,
+        marginTop: 20,
+        width: '100%',
+        height: 55,
+        letterSpacing: 0.5,
+    },
+    forgetCode: {
+        color: '#33CC66',
+        textAlign: 'right',
+        fontSize: 12,
+        fontWeight: '400',
+        marginTop: 13,
+        fontStyle: 'normal',
+        letterSpacing: 0.5,
+    },
+    buttonLogin: {
+        backgroundColor: '#33CC66',
+        padding: 15,
+        textAlign: 'left',
+        paddingLeft: 80,
+        paddingRight: 80,
+        borderRadius: 5,
+        marginTop: 20,
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 14,
+        letterSpacing: 0.5,
+    },
+    textSignUp: {
+        textAlign: 'center',
+        fontSize: 12,
+        color: '#A3A3A3',
+        marginTop: 18,
+        fontWeight: '400',
+        letterSpacing: 0.5,
+    },
+    textSignUpLink: {
+        color: '#33CC66',
+        fontWeight: "500",
+    },
+    error: {
+        color: 'red',
+        fontSize: 12,
+        marginTop: 5,
+        fontStyle: 'normal',
+        letterSpacing: 0.5,
+    },
+    textBottom: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+      letterSpacing: 0.5,
   },
   textBottomDesc: {
-    textAlign: "center",
-    fontSize: 10,
-    color: "#A3A3A3",
-    marginTop: 10,
-    marginHorizontal: 20,
+      textAlign: 'center',
+      fontSize: 9,
+      color: '#A3A3A3',
+      marginHorizontal: 10,
+      fontWeight: '400',
+      lineHeight: 12,
+      letterSpacing: 0.5,
   },
 });
