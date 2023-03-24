8b181ada6e31b3b75e51123d0fcc4a1d9fd9343d
missiria <missiria@gmail.com>
2023-03-24 13:06:06 +0000

    SPRINT STOPPED

--- Manager/src/app/modules/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export {AuthPage} from './AuthPage'
-export * from './redux/AuthRedux'
-export * from './AuthPage'
-export * from './Logout'
-export * from './redux/AuthInit'
--- Manager/src/app/modules/auth/models/AuthModel.ts
+++
@@ -1,4 +0,0 @@
-export interface AuthModel {
-  api_token: string
-  refreshToken?: string
-}
